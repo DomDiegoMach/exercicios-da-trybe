ef42dbff2a130bb23dcf1c33323ac081d2074fbb
Diego Machado <diegomachado.ene@gmail.com>
2021-12-05 04:19:06 -0300

    Treinando Objetos

---
+++ fundamentos/bloco-04-Introducao-a-Javascript-e-logica-de-programacao/Dia-04-Objetos-e-Funcoes/exercicios-dia4-4.js
@@ -0,0 +1,10 @@
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 }
+};
+for(let key in player){
+    console.log(key);
+    }
+    console.log(player.name);
