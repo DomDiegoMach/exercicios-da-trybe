bec16a179110c6fcc12760356a5a94d4add5fae0
Diego Machado <diegomachado.ene@gmail.com>
2021-12-13 15:38:39 -0300

    Parte Introdutoria do 5.1 revisada

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,30 @@
+let titleback = document.querySelectorAll('h1')[0];
+titleback.style.backgroundColor = "limegreen";
+titleback.style.height = "50px"
+
+let backgeral = document.querySelector('#container');
+backgeral.style.backgroundColor = "whitesmoke";
+
+let urgentes = document.querySelectorAll('.emergency-tasks')[0];
+urgentes.style.backgroundColor = "salmon";
+
+let headUrgentesImp = document.querySelectorAll('h3')[0];
+headUrgentesImp.style.backgroundColor = "purple";
+
+let headUrgentesNaoImp = document.querySelectorAll('h3')[1];
+headUrgentesNaoImp.style.backgroundColor = "purple";
+
+let naoUrgentes = document.querySelectorAll('.no-emergency-tasks')[0];
+naoUrgentes.style.backgroundColor = "yellow";
+
+let headNaoUrgentesImp = document.querySelectorAll('h3')[2];
+headNaoUrgentesImp.style.backgroundColor = "black";
+
+let headNaoUrgentesNaoImp = document.querySelectorAll('h3')[3];
+headNaoUrgentesNaoImp.style.backgroundColor = "black";
+
+let rodape = document.querySelectorAll('div')[4];
+rodape.style.backgroundColor = "darkgreen";
+rodape.style.height = '30px';
+rodape.style.textAlign = 'center';
+rodape.style.textAlign = 'middle';
