baeb548461b6a0bf64ba82aa1965102e44eb49c0
Diego Machado <diegomachado.ene@gmail.com>
2021-12-15 13:35:19 -0300

    Estudar mais a  parte 5.3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Parte 1/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Parte 1/main.js
@@ -6,21 +6,51 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 
 // 1. Copie esse arquivo e edite apenas ele;
-// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
+// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página 
+// no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function adicionaTech(event) {
+  let techElement = document.querySelector('.tech');
+  techElement.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+
+firstLi.addEventListener('click', adicionaTech);
+secondLi.addEventListener('click', adicionaTech);
+thirdLi.addEventListener('click', adicionaTech);
+
+
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+input.addEventListener('input', function(event) { 
+  let techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
+  });
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+myWebpage.addEventListener('dblclick', function() {
+  window.location.replace('https://blog.betrybe.com/');
+});
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+myWebpage.addEventListener('mouseover', function(event) {
+  event.target.style.color = 'red';
+});
+
+myWebpage.addEventListener('mouseout', function(event) {
+  event.target.style.color = 'unset';
+});
 
 // Segue abaixo um exemplo do uso de event.target:
 
@@ -31,9 +61,9 @@ function resetText(event) {
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
-
 firstLi.addEventListener('dblclick', resetText);
-
+secondLi.addEventListener('dblclick', resetText);
+thirdLi.addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'firstLi'.
