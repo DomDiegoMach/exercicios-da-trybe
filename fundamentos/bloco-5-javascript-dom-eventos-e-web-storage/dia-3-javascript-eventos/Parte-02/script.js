e90fb1da55bb9c40f44ac6521d2ed3fc169e7d9a
Diego Machado <diegomachado.ene@gmail.com>
2021-12-15 16:45:30 -0300

    Estudar mais a  parte 5.3-2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Parte-02/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Parte-02/script.js
@@ -1,16 +1,48 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
+    let weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+    let weekDaysList = document.querySelector('.week-days');
   
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
+    for (let i = 0; i < weekDays.length; i += 1) {
+      let days = weekDays[i];
+      let dayListItem = document.createElement('li');
       dayListItem.innerHTML = days;
-  
       weekDaysList.appendChild(dayListItem);
-    };
-  };
+    }
+  }
   
+   // Escreva seu código abaixo.
+
   createDaysOfTheWeek();
+
+  let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+                     17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfTheMonth() {
+  let getDaysList = document.querySelector('#days');
+
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let day = dezDaysList[i];
+    let dayItem = document.createElement('li');
+
+    if (day === 24 | day === 31) {
+      dayItem.className = 'day holiday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);
+    } else if (day === 4 | day === 11 | day === 18) {
+      dayItem.className = 'day friday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);
+    } else if (day === 25) {
+      dayItem.className = 'day holiday friday';
+      dayItem.innerHTML = day;
+      getDaysList.appendChild(dayItem);
+    } else {
+      dayItem.innerHTML = day;
+      dayItem.className = 'day';
+      getDaysList.appendChild(dayItem);
+    }
+  };
+};
+createDaysOfTheMonth();
   
   // Escreva seu código abaixo.
