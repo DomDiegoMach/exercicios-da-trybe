66d6e9b6870f8ce8ed5ac75a788d8e487c972d19
Diego Machado <diegomachado.ene@gmail.com>
2022-01-14 11:11:59 -0300

    Iniciando aula 07

---
+++ fundamentos/Bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Dia-01-JavaScript-ES6-let-const-arrow-functions-e-template-literals/Parte-01/script.js
@@ -0,0 +1,21 @@
+function testingScope(escopo) {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
+
+  /**
+   Agora você vai fazer alguns exercícios de fixação.
+Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+Modifique a estrutura da função para que ela seja uma arrow function .
+Modifique as concatenações para template literals .
+Copie o código abaixo.
+   */
