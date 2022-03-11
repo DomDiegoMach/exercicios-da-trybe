0bf389e033d154cef70cbefd46f2953482984577
Diego Machado <diegomachado.ene@gmail.com>
2022-03-11 18:30:28 -0300

    Revisando Bloco 11 dia 1

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      clicksBtnOne: 0,
+      clicksBtnTwo: 0,
+      clicksBtnThree: 0,
+    };
+
+    this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.handleButtonTwo = this.handleButtonTwo.bind(this);
+    this.handleButtonThree = this.handleButtonThree.bind(this);
+  };
+
+  handleButtonOne() {
+    this.setState(({ clicksBtnOne }) => ({clicksBtnOne: clicksBtnOne + 1,}) , () => {
+      console.log(`Botão 1 ${this.getButtonColor(this.state.clicksBtnOne)}`);
+    })};
+  
+  handleButtonTwo() {
+    this.setState(({ clicksBtnTwo }) => ({
+      clicksBtnTwo: clicksBtnTwo + 1,
+    }) , () => {
+      console.log(`Botão 2 ${this.getButtonColor(this.state.clicksBtnTwo)}`);
+    });
+  }
+  
+  handleButtonThree() {
+    this.setState(({ clicksBtnThree }) => ({
+      clicksBtnThree: clicksBtnThree + 1,
+    }) , () => {
+      console.log(`Botão 2 ${this.getButtonColor(this.state.clicksBtnThree)}`);
+    });
+  }
+
+  // Para essa função, não precisamos utilizar o bind porque ela é utilizada
+  // apenas dentro do contexto do componente App
+  getButtonColor(num) {
+    // Essa função contém um ternário que realiza a lógica para mudarmos
+    // a cor do botão para verde quando for um número par
+    return num % 2 === 0 ? 'green' : 'red';
+  }
+
+  render() {
+    const { clicksBtnOne, clicksBtnTwo, clicksBtnThree } = this.state;
+    return (
+      <div className='container'>
+        <button
+          onClick={ this.handleButtonOne }
+          // Para renderizarmos as cores, precisamos acrescentar a função
+          // que contém a nossa lógica ao "inline style", passando o estado
+          // correspondente como parâmetro
+          style={{ backgroundColor: this.getButtonColor(clicksBtnOne) }}
+        >
+          Botão 1 | Count = { clicksBtnOne }
+        </button>
+        <button
+          onClick={ this.handleButtonTwo }
+          style={{ backgroundColor: this.getButtonColor(clicksBtnTwo) }}
+        >
+          Botão 2 | Count = { clicksBtnTwo }
+        </button>
+        <button
+          onClick={ this.handleButtonThree }
+          style={{ backgroundColor: this.getButtonColor(clicksBtnThree) }}
+        >
+          Botão 3 | Count = { clicksBtnThree }
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
